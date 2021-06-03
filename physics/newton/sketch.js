9d0474bf06fdc168d24b47511f27f285c5b27107
pattex16 <iacobuccivalerio@gmail.com>
2021-06-03 13:07:09 +0200

    modified:   physics/newton/sketch.js

--- physics/newton/sketch.js
+++ physics/newton/sketch.js
@@ -2,11 +2,10 @@ function setup() {
   createCanvas(512, 512);
   background(255);
 
-  noLoop();
+}
+function draw() {
   loadPixels();
   for (let x = 0 ; x < 512*512*4*4 ; x++)
     pixels[x] = random(255);
   updatePixels();
-}
-function draw() {
 }
